ffb7271736835f07eb752a1af81a2467cb88eee0
Bernd Wissmeier <ajodochus@gmail.com>
2022-07-28 14:21:41 +0200

    add tng in testconfig

---
+++ testConfig.ts
@@ -0,0 +1,15 @@
+export const testConfig = {
+    qa: `http://automationpractice.com`,
+    dev: ``,
+    qaApi: `https://reqres.in`,
+    devApi: ``,
+    username: `demouat@gmail.com`,
+    password: `U2FsdGVkX1+6691EJeW/QG2vpjUvj47EQBrTIN5BqZg=`,
+    waitForElement: 120000,
+    dbUsername: ``,
+    dbPassword: ``,
+    dbServerName: ``,
+    dbPort: ``,
+    dbName: ``,
+    tng: `https://review-create-doc-pn8ux3.kube.devtng.com/`
+}
